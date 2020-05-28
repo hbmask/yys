32ac2070ee09b0be72daf304dcea1c1425794cc3
hbmask <javae@qq.com>
2020-05-28 13:47:47 +0800

    房主战斗中开御魂
    房主战斗中开御魂

--- fangzhu.go
+++ fangzhu.go
@@ -17,6 +17,39 @@ func (f *TFMain) YuHunOrJueXingFangZhu(i int,fp flagpiex.FLagPiex){
 			}//点击准备
 			time.Sleep(time.Millisecond*300)
 		}
+		if fp.FlagZhanDouJieMianJiaCeng(){//战斗界面->点击加层
+			if  f.YuHunBuffFlag ==false{//御魂buff状态
+				f.DJ_Click_Range(106,595,26,25,"加层检查")
+				for  {
+					if fp.FlagYuHunBuffRed(){//红色状态
+						f.DJ_Click_Range(701,199,20,6,"开启御魂buff")
+						f.YuHunBuffFlag =true
+						f.DJ_Click_Range(0,489,600,30,"")
+						//time.Sleep(time.Millisecond*500)
+						f.StopYuHunNum=0
+						f.OffBuff=0
+						break
+					}
+					if fp.FlagYuHunBuffGold(){//金色状态
+						//f.DJ_Click_Range(317,489,600,61,"御魂buff已打开")
+						f.YuHunBuffFlag =true
+						f.DJ_Click_Range(0,489,600,30,"buff已经开启")
+						//time.Sleep(time.Millisecond*500)
+						//f.DJ_Click_Range(0,489,600,30,"")
+						f.StopYuHunNum=0
+						f.OffBuff=0
+						break
+					}
+					f.StopYuHunNum++
+					if f.StopYuHunNum>=20{
+						f.StopYuHunNum=0
+						break
+					}
+					f.StopYuHunNum++
+					time.Sleep(time.Millisecond*50)
+				}
+			}
+		}
 		//战斗界面
 		if fp.FlagZhanDouJieMian(){
 			//显示一回木 点大舅妈
@@ -40,9 +73,9 @@ func (f *TFMain) YuHunOrJueXingFangZhu(i int,fp flagpiex.FLagPiex){
 		}
 		//在 庭院 探索 房间 //60秒没动作关闭御魂buff
 		if fp.FlagTingYuan()||fp.FlagTanSuo()||fp.FlagYuHunJueXingFangJian(){
-			if  f.YuHunBuffFlag ==false{//御魂buff状态
-				f.YuHunOnBuffJianCha() //选择御魂是否打开御魂buff
-			}
+			//if  f.YuHunBuffFlag ==false{//御魂buff状态
+			//	f.YuHunOnBuffJianCha() //选择御魂是否打开御魂buff
+			//}
 			if  f.OffBuff>=90||f.OffNumGame==0{//记录副本次
 				f.YuHunTingYuanOffBuffJianCha()
 				f.YuHunOffBuffJianCha()
